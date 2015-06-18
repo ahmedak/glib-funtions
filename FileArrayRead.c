14794841d6eec08308a0aeb342782cf6f6da5ba5
mkanz <k4kanz@gmail.com>
2015-06-18 17:51:57 +0530

    Added search
    version 2

--- FileArrayRead.c
+++ FileArrayRead.c
@@ -8,8 +8,18 @@ gint int_comparator(gconstpointer item1, gconstpointer item2) {
 void print_array(GArray * a) {
 	int i;
 	for(i=0; i<(a->len); i++)
-		g_printf("%d ",g_array_index(a, int, i));
+		g_printf("%d  ",g_array_index(a, int, i));
 	g_printf("\n");
+}
+
+void find_element(int x,GArray* a){
+	int i;
+	for(i=0;i<a->len;i++)
+		if(g_array_index(a,int,i)==x){
+			g_printf("\nElement found at position %d\n",i);
+			return;
+		}
+	printf("\nElement not found in the array\n");
 }
 
 int main(int argc, char ** argv) {
@@ -20,7 +30,7 @@ int main(int argc, char ** argv) {
         if(bl==TRUE) {
                 GArray * array = g_array_new(FALSE, FALSE, sizeof(int));
                 gchar **p = g_strsplit(fcontents,"\n",0);
-                int i;
+                int i,x;
 		for(i=0; p[i]!=NULL; i++){
 			if(g_ascii_strcasecmp(p[i], "")){
 				int r = g_strtod(p[i], NULL);
@@ -28,9 +38,13 @@ int main(int argc, char ** argv) {
 			}
 		}
 		g_array_sort(array, (GCompareFunc)int_comparator);
+		g_printf("\nEnter the number to searched for:  ");
+		scanf("%d",&x);
+		find_element(x,array);
+		g_printf("\nSorted Array: \n");
 		print_array(array);
-                g_printf("\n\nMaximum element: %d\n", g_array_index(array, int, (array->len)-1));
-                g_printf("Number of Elements in Array: %d \n\n", array->len);
+                g_printf("\nMaximum element: %d\n", g_array_index(array, int, (array->len)-1));
+                g_printf("\nNumber of Elements in Array: %d \n", array->len);
         }
         else g_printf("Reading Data Failed\n");
         return 0;
