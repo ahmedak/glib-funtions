3ba0e2f0e6efda4fba72605bb2b0f775aba9377b
mkanz <k4kanz@gmail.com>
2015-06-23 13:37:14 +0530

    Start/End timer, Get elapsed time, Reset timer
    version 1

---
+++ Timer.c
@@ -0,0 +1,26 @@
+#include <glib.h>
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+	double elapsed;
+	char key;
+	GTimer * timer = g_timer_new();
+	while(1){
+		printf("\nTimer started. Press any key to stop timer: ");
+		getchar();
+		getchar();
+		g_timer_stop(timer);
+		elapsed = g_timer_elapsed(timer, NULL);
+		printf("Time elapsed: %fs\n", elapsed);
+		printf("\nPress 'r' to reset timer, any other key to exit: ");	
+		scanf("%c", &key);
+		if(key=='r'){
+			g_timer_reset(timer);
+			getchar();
+		}	
+		else
+			break;
+	}
+	g_timer_destroy(timer);
+	return 0;
+}
