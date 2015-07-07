804abff0b47637c0a7a1f6fa16e542aba656d473
Ahmed Karanath <ahmedabdulk@gmail.com>
2015-07-07 19:34:56 +0530

    modified: Timer.c
    Added comments.

--- Timer.c
+++ Timer.c
@@ -4,17 +4,24 @@
 int main(int argc, char ** argv) {
 	double elapsed;
 	char key;
+	
+	// Create new Gtimer variable
 	GTimer * timer = g_timer_new();
 	while(1){
 		printf("\nTimer started. Press any key to stop timer: ");
 		getchar();
 		getchar();
+
+		// Stop timer
 		g_timer_stop(timer);
+
+		// Elapsed time
 		elapsed = g_timer_elapsed(timer, NULL);
 		printf("Time Elapsed: %fs\n", elapsed);
 		printf("\nPress 'r' to reset timer, any other key to exit: ");	
 		scanf("%c", &key);
 		if(key=='r'){
+			// Reset timer
 			g_timer_reset(timer);
 			getchar();
 		}	
