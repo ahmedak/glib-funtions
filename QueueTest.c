983959ec3c7cef54b0b235cd47e089fd6697eb63
Ahmed Karanath <ahmedabdulk@gmail.com>
2015-07-07 19:10:11 +0530

    modified: Queue.c & QueueTest.c
    Modified function names to make them uniform and created ’queue’ data
    type for ease of use.

--- QueueTest.c
+++ QueueTest.c
@@ -3,31 +3,31 @@
 #include <stdio.h>
 #include <glib.h>
 
-void queueInfo(GQueue* qqq) {
-	printf("The head of the queue is %s\n",show_head(qqq));
-	printf("The tail of the queue is %s\n",show_tail(qqq));
+void queueInfo(queue* qqq) {
+	printf("The head of the queue is %s\n",queue_show_head(qqq));
+	printf("The tail of the queue is %s\n",queue_show_tail(qqq));
 	printf("The length of the queue is %d\n",queue_length(qqq));
 } // end queueInfo()
 
 int main() {
-	GQueue* qq = NULL;
+	queue* qq = NULL;
 	char* ch;
 
-	qq =  g_queue_new();
+	qq =  queue_new();
 	printf("Enquing a\n");
-	enqueue(qq, "a");
+	queue_enqueue(qq, "a");
 	printf("Enquing b\n");
-	enqueue(qq, "b");
+	queue_enqueue(qq, "b");
 	printf("Enquing c\n");
-	enqueue(qq, "c");
+	queue_enqueue(qq, "c");
 	printf("Enquing d\n");
-	enqueue(qq, "d");
+	queue_enqueue(qq, "d");
 	printf("Enquing e\n");
-	enqueue(qq, "e");
+	queue_enqueue(qq, "e");
 	queueInfo(qq);
 
 	printf("Dequeing\n");
-	ch = dequeue(qq);
+	ch = queue_dequeue(qq);
 	printf("The dequeued char is %s\n",ch);
 	queueInfo(qq);
 
