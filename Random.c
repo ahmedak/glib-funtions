628a50296b27a66fc639c3aa9df8e8c28247f914
Ahmed Karanath <ahmedabdulk@gmail.com>
2015-07-07 19:13:59 +0530

    modified: Random.c
    Added comments.

--- Random.c
+++ Random.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 
 int main(int argc, char ** argv){
+	
+	// Random number between 0 & 1
 	double r = g_random_double();
+	printf("Random number between 0 & 1: %f",r);
+	
+	// Random integer between a & b
 	int a,b;
 	printf("\nEnter 'a' and 'b' : \n");
 	scanf("%i%i", &a,&b);
 	int rr = g_random_int_range(a+1,b);
-	printf("Random number between 0 & 1: %f",r);
 	printf("\nRandom number between %d and %d: %d\n", a, b, rr);
 	return 0;
 }
